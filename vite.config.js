e9b5d80cebaf98a7b6878ab8eb05ab2e006b25b6
SEAB KOEURN <seabkoeurn64@gmail.com>
2025-11-02 23:42:36 +0700

    Update build and Vercel config for SPA deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Removed `base` since Vercel serves from root
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
